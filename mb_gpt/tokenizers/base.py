0c72a3a67f40438644df46e834a63f19f7c3eb8f
Malav <malav.b93@gmail.com>
2024-12-25 07:54:18 +0000

    updated version.py to 1.0.3

--- mb_gpt/tokenizers/base.py
+++ mb_gpt/tokenizers/base.py
@@ -5,9 +5,10 @@ Main tokenizer class for the GPT model.
 import regex as re
 from typing import List, Optional, Tuple, Union
 import tiktoken
+from PIL import Image
 # import sentencepiece as spm
 
-__all__ = []
+__all__ = ["Tokenizer", "VITokenizer"]
 
 SPLIT_REGEX1 = re.compile(r"""'s|'t|'re|'ve|'m|'ll|'d| ?\p{L}+|\p{N}+|[^\s\p{L}\p{N}]+|\s+""")
 SPLIT_REGEX2 = re.compile(r"""[\w'-]+|[^\w\s]+|\s+""")
@@ -113,19 +114,34 @@ class Tokenizer:
             return self._decode_tokens(tokens, self.vocab)
         return self.enc.decode(tokens)
 
+
+class VITokenizer():
+    """ 
+    Tokenizer for Vision and Language tasks.
+    """
+    def __init__(self) -> None:
+        pass
+
+    def _read_image(self, image: str) -> Image:
+        """
+        Read an image.
+        """
+        if isinstance(image, Image.Image):
+            return image
+        return Image.open(image)
+
+    def _to_patches(self, image: str, patch_size: int) -> List[str]:
+        """
+        Convert an image to patches.
+        """
+        pass
+
+
     def _convert_image_to_tokens(self, image: str) -> List[str]:
         """
         Convert an image to a list of tokens.
         """
         pass
-
-
-    def _convert_audio_to_tokens(self, audio: str) -> List[str]:
-        """
-        Convert an audio to a list of tokens.
-        """
-        pass
-
 
     def _convert_tokens_to_image(self, tokens: List[str]) -> str:
         """
@@ -134,8 +150,21 @@ class Tokenizer:
         pass
 
 
+class AudioTokenizers():
+    """
+    Tokenizers for audio data.
+    """
+    def __init__(self) -> None:
+        pass
+
+    def _convert_audio_to_tokens(self, audio: str) -> List[str]:
+        """
+        Convert audio to a list of tokens.
+        """
+        pass
+
     def _convert_tokens_to_audio(self, tokens: List[str]) -> str:
         """
-        Convert tokens to an audio.
+        Convert tokens to audio.
         """
         pass
